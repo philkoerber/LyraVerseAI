10f0c65ba36d1b51035a781121ed9eca60de6981
philkoerber <philipp.koerber.93@gmail.com>
2023-07-27 18:00:15 +0900

    work on supabase insert

--- app/account/create/lyric/Lyric.jsx
+++ app/account/create/lyric/Lyric.jsx
@@ -1,9 +1,7 @@
 "use client"
 
 import React, { useEffect, useCallback } from 'react';
-import { Reorder } from "framer-motion";
 import { useState } from 'react';
-import ButtonGroup from './ButtonGroup';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import ReorderGroup from './ReorderGroup';
 
@@ -16,43 +14,33 @@ function Lyric({ lyric, session }) {
     const [username, setUsername] = useState()
     const [loading, setLoading] = useState()
         const [updateTimeout, setUpdateTimeout] = useState(null);
-
-
-
-    const getProfile = useCallback(async () => {
-    try {
-      setLoading(true)
-
-      let { data, error, status } = await supabase
-        .from('profiles')
-        // .select(`username, website, avatar_url`)
-        .select('username, lyrics')
-        .eq('id', user?.id)
-        .single()
-
-      if (error && status !== 406) {
-        throw error
-      }
-
-      if (data) {
-        setUsername(data.username)
-      }
-    } catch (error) {
-      alert('Error loading user data!')
-    } finally {
-      setLoading(false)
-    }
-  }, [user, supabase])
-
-  useEffect(() => {
-    getProfile()
-  }, [user, getProfile])
     
     // Dummy update function
-    const updateLyric = () => {
-        console.log("Update function called!");
-        // Replace this with your actual update logic
+    const updateLyric = async (items) => {
+        
+        console.log("updating lyrics...")
+        const { error } = supabase
+            .from("lyrics")
+            .insert([{}])
     };
+
+//      async function updateProfile({ username}) {
+//     try {
+//       setLoading(true)
+
+//       let { error } = await supabase.from('profiles').upsert({
+//         id: user?.id,
+//         username,
+//         updated_at: new Date().toISOString(),
+//       })
+//       if (error) throw error
+//       alert('Profile updated!')
+//     } catch (error) {
+//       alert('Error updating the data!')
+//     } finally {
+//       setLoading(false)
+//     }
+//   }
 
     const handleReorder = (items) => {
         setItems(items);
@@ -64,15 +52,13 @@ function Lyric({ lyric, session }) {
 
         // Set a new timeout to call the update function after 500ms
         setUpdateTimeout(setTimeout(() => {
-            // Call your update function here
-            // Replace the following line with your actual update function call
-            updateLyric();
+            updateLyric(items);
         }, 500));
     };
 
   const handleTextChange = (itemToUpdate, newValue) => {
         if (itemToUpdate === newValue) {
-            return; // If they are the same, return early, and no update is necessary
+            return; // If they are the same, no update is necessary
         }
 
         const itemIndex = items.indexOf(itemToUpdate);
@@ -85,7 +71,10 @@ function Lyric({ lyric, session }) {
     };
 
     return (
-        <ReorderGroup items={items} handleReorder={handleReorder} handleTextChange={handleTextChange} />
+        <ReorderGroup
+            items={items}
+            handleReorder={handleReorder}
+            handleTextChange={handleTextChange} />
     )
 }
 
