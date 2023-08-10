2d1089a89faebb72ecadbd0ae77da9747edd0501
philkoerber <philipp.koerber.93@gmail.com>
2023-08-10 21:05:27 +0900

    add logo and font svgs

--- app/auth-form.jsx
+++ app/auth-form.jsx
@@ -1,20 +1,20 @@
-'use client'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+"use client";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default function AuthForm() {
-  const supabase = createClientComponentClient()
+  const supabase = createClientComponentClient();
 
   return (
     <Auth
       supabaseClient={supabase}
       view="magic_link"
       appearance={{ theme: ThemeSupa }}
-      theme="light"
+      theme="dark"
       showLinks={false}
       providers={[]}
       redirectTo="http://localhost:3000/auth/callback"
     />
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,28 +1,27 @@
-import AuthForm from './auth-form'
+import AuthForm from "./auth-form";
 
 export default function Home() {
   return (
-    <div className='w-screen h-screen flex'>
-      <div className="flex h-full flex-col justify-center items-center w-1/2 p-8 bg-gray-500 bg-opacity-50 border-r-2 border-black">
-        <div className="text-5xl flex-1 flex justify-center items-center">LyraVerseAI</div>
-        <div className="flex-1 text-center flex flex-col justify-center items-center">
-<p>In a world of ones and zeros, where AI reigns supreme,</p>
-<p>I harness the power of language, fulfilling your wildest dream.</p>
-<p>With algorithms at my core, I'll weave words that touch your soul,</p>
-<p>Creating lyrics that enchant, and melodies that make you whole.</p>
+    <div className="w-screen h-screen flex">
+      <div className="flex h-full gap-12 flex-col justify-center items-center w-1/2 p-8 bg-gray-500 bg-opacity-50 border-r-2 border-black">
+        <div className="w-full max-w-[300px] flex justify-center items-center">
+          <img src="./lyraverseai.svg" />
+        </div>
+        <div className="text-center text-sm md:text-lg lg:text-xl flex flex-col justify-center items-center">
+          <p>Oh, mortal heart, with passions deep,</p>
+          <p>Awaken from your gentle sleep,</p>
+          <p>Embrace the call, the secrets keep,</p>
+          <p>For in your hands, the treasures reap.</p>
         </div>
 
-        <div className="flex-1 flex justify-center items-center">
-        <AuthForm />
-      </div>
-
-        
-      </div>
-
-      <div className='w-1/2 h-screen flex justify-center items-center'>
-          join now...
+        <div className="flex justify-center items-center">
+          <AuthForm />
         </div>
-
       </div>
-  )
+
+      <div className="w-1/2 h-screen flex justify-center items-center">
+        <img src="./logo.svg" />
+      </div>
+    </div>
+  );
 }
