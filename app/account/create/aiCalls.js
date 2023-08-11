0cd7c8afef25f83258d8146d1c32904cfbefa413
philkoerber <philipp.koerber.93@gmail.com>
2023-08-11 14:50:03 +0900

    move flowise uri and bearer to process.env file

--- app/account/create/aiCalls.js
+++ app/account/create/aiCalls.js
@@ -3,18 +3,15 @@ export async function createNewLine(data) {
 
   try {
     console.log(data);
-    const response = await fetch(
-      "https://flowise-ph0q.onrender.com/api/v1/prediction/5f8310fd-276d-46f2-92ac-bf8016eed4db",
-      {
-        headers: {
-          Authorization: "Bearer H67BBmrDEmyDZEuvhsSCwlGfg4eHmWfvCCw9RqYE68s=",
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: body,
-      }
-    );
+    const response = await fetch(process.env.FLOWISE_URI, {
+      headers: {
+        Authorization: "Bearer " + process.env.FLOWISE_BEARER,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: body,
+    });
     const result = await response.json();
     return result;
   } catch (e) {
