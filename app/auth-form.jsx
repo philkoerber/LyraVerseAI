2647c042274740a6e514e6d6ef5028bf578359a0
philkoerber <philipp.koerber.93@gmail.com>
2023-08-11 15:08:30 +0900

    update callback url to production

--- app/auth-form.jsx
+++ app/auth-form.jsx
@@ -14,7 +14,7 @@ export default function AuthForm() {
       theme="dark"
       showLinks={false}
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo="https://lyraverse-ai.vercel.app/auth/callback"
     />
   );
 }
